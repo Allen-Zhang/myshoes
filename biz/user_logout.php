42c719f60847d27bd5741ca191e81bfe39b375ef
Allen Yingyuan Zhang <zyy2013@bu.edu>
2014-03-19 18:50:09 -0400

    Test

--- biz/user_logout.php
+++
@@ -1,8 +0,0 @@
-
-<!-- For users logout -->
-<?php
-	session_start();
-	session_unset();
-	session_destroy();
-	header( 'Location: ../index.php' );
-?>
--- footer.php
+++
@@ -1,6 +0,0 @@
-<!-- Footer layout of the website  -->
-<div class="footer">
-    <hr>
-    <p>Copyright &copy; <?php echo date("Y"); ?> MyShoes. All rights reserved</p>
-</div>
-
